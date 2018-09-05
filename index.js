926ce72ad83c978814ff15504b21d3f0238476b9
nooha mohammed <noohamohammed00@gmail.com>
2018-09-05 21:07:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,17 @@
 function lowerCaseDrivers(list){
   return list.map((name)=> name.toLowerCase())
 }
+function nameToAttributes(list){
+  return list.map(function (driver){
+    let first = driver.split(' ')[0];
+    let second = driver.split(' ')[1];
+    return {firstName: first, lastName:second}});}
+
+
+function attributesToPhrase(list){
+  return list.map(function(driverDetails){
+    let name = driverDetails.name;
+    let home = driverDetails.hometown;
+    return `${name} is from ${home}`
+  })
+}
